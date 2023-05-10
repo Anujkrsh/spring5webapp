7f5abb1709c5f4b6c576027ca5d667bd31e3c618
Sharma <anuj.kumar.a.sharma@accenture.com>
2023-05-10 23:01:37 +0530

    Update changes

---
+++ src/main/java/guru/springframework/spring5webapp/Domain/Book.java
@@ -0,0 +1,76 @@
+package guru.springframework.spring5webapp.Domain;
+
+import javax.persistence.*;
+import java.util.*;
+
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String title;
+    private String isbn;
+    @ManyToMany
+    @JoinTable(name = "authur_book",joinColumns = @JoinColumn(name = "book_id"),
+                inverseJoinColumns = @JoinColumn(name = "authur_id"))
+    private Set<Author> author = new HashSet<>();
+
+    public Book() {
+
+    }
+
+    public Book(String title, String isbn/*, Set<Author> author*/) {
+        this.title = title;
+        this.isbn = isbn;
+      //  this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Set<Author> author) {
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id == book.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
+}
